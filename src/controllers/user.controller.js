5464c982622740d6397dd6d1a2ec76277916fafa
shivambarpetejii <barpetejii@gmail.com>
2025-05-13 20:13:21 +0530

    ok

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -256,6 +256,26 @@ const refereshAccessToken = asyncHandler(async(req,res)=>{
 })
 
 
+const changeCurrentPassword = asyncHandler(async(req,res)=>{
+    const {oldPassword, newPassword} = req.body
+   const user =  await User.findById(req.user?.id)
+   const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+   if(!isPasswordCorrect)
+   {
+    throw new ApiError(400,"Invalid old password")
+
+   }
+    user.password = newPassword;
+
+    await user.save({validateBeforeSave:false})
+
+    return res.status(200).json(new ApiResponse(200,{},"password change succfully"))
+
+})
+
+
+
+
 const getCurrentUser = asyncHandler(async (req, res) => {
     return res.status(200).json(
       new ApiResponse(200, req.user, "Current user fetched successfully")
